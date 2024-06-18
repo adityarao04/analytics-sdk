763b04d0b05fcfd4c37cc9868d525e6bdacfb909
Aditya Rao <adityarao@Admins-MacBook-Pro.local>
2024-06-18 13:50:36 +0530

    umd added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import sumOfTwo from "./sumOfTwo";
 
 
-
 const LibText = {
     sumOfTwo: sumOfTwo
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     // where should code be bundled
     output:{
        filename: 'index.js',
+       libraryTarget: "umd",
        path: path.resolve(__dirname, 'dist'),
     }
 }
